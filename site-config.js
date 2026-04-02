f42b6d444674846d975830ca0c35bb0b7483efa2
sosorke <sosorwvk@gmail.com>
2026-04-02 09:44:43 -0300

    Update site-config.js

--- site-config.js
+++ site-config.js
@@ -1,4 +1,4 @@
 window.CLINICA_CONFIG = {
-  googleAppsScriptUrl: "https://script.google.com/macros/s/AKfycbzotjCwo2vPVi8qCy05tuHl5O6HicJtAVHx67TuJTDLy5uCiJGQYW36ONBZd2kkutHp/exec",
+  googleAppsScriptUrl:"https://script.google.com/macros/s/AKfycbyPLePmFTdcxU82TJ2M0RAiS_buU3Y4xqxP7qgM6unYvgT__RDkA8A4ZweINKLoGGxfyQ/exec",
   googleSheetUrl: ""
 };
